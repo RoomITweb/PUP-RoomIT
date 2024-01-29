18b9b2a9c556076f8fecdde9f2f404c3281eb8a8
RoomITweb <151912982+RoomITweb@users.noreply.github.com>
2024-01-30 04:28:27 +0800

    sana tama ka na

--- roomit/src/Components/F_classSched.js
+++ roomit/src/Components/F_classSched.js
@@ -5,7 +5,6 @@ import { app } from './firebase';
 import ReactModal from 'react-modal';
 import BarcodeScanner from './BarcodeScanner';
 
-
 function FacultySchedule() {
   const [facultyName, setFacultyName] = useState('');
   const [facultySchedules, setFacultySchedules] = useState([]);
@@ -22,7 +21,10 @@ function FacultySchedule() {
   const [errorMessage, setErrorMessage] = useState('');
   const [successMessage, setSuccessMessage] = useState('');
   // Retrieve the last known roomOccupied state from localStorage
-  const initialRoomOccupiedState = localStorage.getItem('roomOccupied') === 'false';
+  const initialRoomOccupiedState = localStorage.getItem('roomOccupied') === 'true';
+  // Retrieve the last known selectedSchedule state from localStorage
+  const initialSelectedScheduleState = JSON.parse(localStorage.getItem('selectedSchedule')) || null;
+
   const [roomOccupied, setRoomOccupied] = useState(initialRoomOccupiedState);
 
   const auth = getAuth(app);
@@ -30,27 +32,26 @@ function FacultySchedule() {
   ReactModal.setAppElement('#root'); // or any other root element in your HTML
 
   navigator.mediaDevices.getUserMedia({ video: true })
-  .then(function (stream) {
-    // Success - the camera is accessible
-  })
-  .catch(function (error) {
-    console.error('Error accessing camera:', error);
-  });
-
+    .then(function (stream) {
+      // Success - the camera is accessible
+    })
+    .catch(function (error) {
+      console.error('Error accessing camera:', error);
+    });
 
   useEffect(() => {
     const fetchData = async (user) => {
       try {
         const userRef = ref(database, `users/${user.uid}`);
         const userSnapshot = await get(userRef);
-  
+
         if (userSnapshot.exists()) {
           const userData = userSnapshot.val();
           if (userData.role === 'faculty') {
             setFacultyName(`${userData.firstName} ${userData.lastName}`);
           }
         }
-  
+
         if (selectedSchoolYear && selectedSemester) {
           const schedulesRef = ref(database, 'schedules');
           const facultyScheduleQuery = query(
@@ -59,7 +60,7 @@ function FacultySchedule() {
             equalTo(facultyName)
           );
           const scheduleSnapshot = await get(facultyScheduleQuery);
-  
+
           if (scheduleSnapshot.exists()) {
             const facultySchedules = [];
             scheduleSnapshot.forEach((schedule) => {
@@ -72,6 +73,20 @@ function FacultySchedule() {
               }
             });
             setFacultySchedules(facultySchedules);
+
+            // Check if there's a selected schedule from localStorage
+            if (initialSelectedScheduleState) {
+              // If there's a selected schedule, check if it's still in the fetched schedules
+              const isStillValid = facultySchedules.some(
+                (schedule) => schedule.room === initialSelectedScheduleState.room
+              );
+
+              if (!isStillValid) {
+                // If not valid, clear the selected schedule
+                setSelectedSchedule(null);
+                localStorage.removeItem('selectedSchedule');
+              }
+            }
           }
         }
       } catch (error) {
@@ -80,15 +95,20 @@ function FacultySchedule() {
         setLoading(false);
       }
     };
-  
+
     const unsubscribe = onAuthStateChanged(auth, (user) => {
       if (user) {
         fetchData(user);
       } else {
+        // Clear the roomOccupied and selectedSchedule when logging out
+        setRoomOccupied(false);
+        setSelectedSchedule(null);
+        localStorage.removeItem('roomOccupied');
+        localStorage.removeItem('selectedSchedule');
         setLoading(false);
       }
     });
-  
+
     return () => {
       unsubscribe();
     };
@@ -98,25 +118,25 @@ function FacultySchedule() {
     setSelectedSchedule(subject);
     setShowScanner(true);
     setIsScannerOpen(true);
-  
+
     if (subject.room) {
       setIsScannerOpen(true);
       setScanMessage(`Room: ${subject.room}`);
     }
   };
-  
+
   const handleQrCodeScan = (result) => {
     // Handle the QR code scan result here
     console.log('QR Code Scan Result:', result);
     setScanResult(result);
-  
+
     if (scanMessage && result.includes(scanMessage)) {
       // Check if the room is already occupied
       if (roomOccupied && selectedSchedule.room !== scanMessage) {
         setErrorMessage('');
         return;
       }
-  
+
       setAttendingClass(true);
       setErrorMessage('');
     } else {
@@ -135,19 +155,19 @@ function FacultySchedule() {
   const handleAttendClass = async () => {
     if (auth.currentUser) {
       const userUid = auth.currentUser.uid;
-  
+
       // Check if the room is already occupied
       const occupiedRoomRef = ref(database, `rooms/${selectedSchedule.room}`);
       const occupiedRoomSnapshot = await get(occupiedRoomRef);
-  
+
       if (occupiedRoomSnapshot.exists()) {
         setErrorMessage('Error: The room is already occupied.');
         return;
       }
-  
+
       // Get the current date and time
       const currentTime = new Date().toLocaleString();
-  
+
       const scheduleData = {
         schoolYear: selectedSchedule.schoolYear,
         semester: selectedSchedule.semester,
@@ -161,24 +181,21 @@ function FacultySchedule() {
         room: selectedSchedule.room,
         attendedTime: currentTime,
       };
-  
+
       // Include the 'attendendTime' field in selectedSchedule
       selectedSchedule.attendTime = currentTime;
-  
+
       await set(ref(database, `rooms/${selectedSchedule.room}`), scheduleData, currentTime);
       await set(ref(database, `users/${userUid}/occupiedRoom`), selectedSchedule.room);
-  
+
       setRoomOccupied(true);
       localStorage.setItem('roomOccupied', 'true');
       setSuccessMessage('You have successfully attended the class.');
       setErrorMessage('');
     }
   };
-  
 
-  
   const handleEndClass = async () => {
-    console.log('Entering handleEndClass');
     setAttendingClass(false);
     setShowScanner(false);
 
@@ -218,10 +235,8 @@ function FacultySchedule() {
 
           await set(ref(database, `rooms/${selectedSchedule.room}`), null);
 
-          console.log('Setting roomOccupied to false');
           setRoomOccupied(false);
-          localStorage.setItem('roomOccupied');
-          console.log('Removed roomOccupied from localStorage');
+          localStorage.setItem('roomOccupied', 'false');
           setErrorMessage('');
           setSuccessMessage('You have successfully ended the class.');
         } catch (error) {
@@ -241,14 +256,6 @@ function FacultySchedule() {
     const filteredSchedules = facultySchedules.filter((schedule) => schedule.day === day);
     setFacultySchedules(filteredSchedules);
   };
-
-  navigator.mediaDevices
-  .getUserMedia({ video: true })
-  .then(function (stream) {
-  })
-  .catch(function (error) {
-    console.error('Error accessing camera:', error);
-  });
 
   const filterSchedulesBySchoolYearAndSemester = (schoolYear, semester) => {
     if (schoolYear === 'All' && semester === 'All') {
@@ -270,140 +277,135 @@ function FacultySchedule() {
   };
 
   return (
-        <div className='h-screen justify-center flex items-center'>
-          <div className="container ">
-          <div className="row">
+    <div className='h-screen justify-center flex items-center'>
+      <div className="container ">
+        <div className="row">
           <div className="col-12">
-          <div className="content-wrapper">
-            <h2 style={{ marginBottom: '20px', textAlign: 'left' }}>My Schedule</h2>
-            <p style={{ fontFamily: 'Bold', marginBottom: '20px' }}>Welcome, {facultyName}!</p>
+            <div className="content-wrapper">
+              <h2 style={{ marginBottom: '20px', textAlign: 'left' }}>My Schedule</h2>
+              <p style={{ fontFamily: 'Bold', marginBottom: '20px' }}>Welcome, {facultyName}!</p>
 
-            <div className="row">
-              <div className="content-wrapper">
-                <div className="form-group">
-                  <select
-                    className="form-control"
-                    onChange={(e) => {
-                      setSelectedSchoolYear(e.target.value);
-                      filterSchedulesBySchoolYearAndSemester(e.target.value, selectedSemester);
-                    }}
-                    value={selectedSchoolYear}
-                  >
-                    <option value="" disabled hidden>Choose a School Year</option>
-                    <option value="All">All</option>
-                    <option value="2022-2023">2022-2023</option>
-                    <option value="2023-2024">2023-2024</option>
-                    {/* Add more options for other school years */}
-                  </select>
+              <div className="row">
+                <div className="content-wrapper">
+                  <div className="form-group">
+                    <select
+                      className="form-control"
+                      onChange={(e) => {
+                        setSelectedSchoolYear(e.target.value);
+                        filterSchedulesBySchoolYearAndSemester(e.target.value, selectedSemester);
+                      }}
+                      value={selectedSchoolYear}
+                    >
+                      <option value="" disabled hidden>Choose a School Year</option>
+                      <option value="All">All</option>
+                      <option value="2022-2023">2022-2023</option>
+                      <option value="2023-2024">2023-2024</option>
+                      {/* Add more options for other school years */}
+                    </select>
+                  </div>
+                </div>
+                <div className="content-wrapper">
+                  <div className="form-group">
+                    <select
+                      className="form-control"
+                      onChange={(e) => {
+                        setSelectedSemester(e.target.value);
+                        filterSchedulesBySchoolYearAndSemester(selectedSchoolYear, e.target.value);
+                      }}
+                      value={selectedSemester}
+                    >
+                      <option value="" disabled hidden>Choose a Semester</option>
+                      <option value="All">All</option>
+                      <option value="1st Semester">1st Semester</option>
+                      <option value="2nd Semester">2nd Semester</option>
+                      <option value="Summer">Summer</option>
+                      {/* Add more options for other semesters */}
+                    </select>
+                  </div>
+                </div>
+                <div className="content-wrapper">
+                  <div className="form-group">
+                    <select
+                      className="form-control"
+                      onChange={(e) => {
+                        setSelectedDay(e.target.value);
+                        filterSchedulesByDay(e.target.value);
+                      }}
+                      value={selectedDay}
+                    >
+                      <option value="" disabled hidden>Choose a Day</option>
+                      <option value="All">All</option>
+                      <option value="Mon/Wed">Mon/Wed</option>
+                      <option value="Tue/Thurs">Tue/Thurs</option>
+                      <option value="Fri/Sat">Fri/Sat</option>
+                      <option value="Monday">Monday</option>
+                      <option value="Tuesday">Tuesday</option>
+                      <option value="Wednesday">Wednesday</option>
+                      <option value="Thursday">Thursday</option>
+                      <option value="Friday">Friday</option>
+                      <option value="Saturday">Saturday</option>
+                      {/* Add more options for other days */}
+                    </select>
+                  </div>
                 </div>
               </div>
-              <div className="content-wrapper">
-                <div className="form-group">
-                  <select
-                    className="form-control"
-                    onChange={(e) => {
-                      setSelectedSemester(e.target.value);
-                      filterSchedulesBySchoolYearAndSemester(selectedSchoolYear, e.target.value);
-                    }}
-                    value={selectedSemester}
-                  >
-                    <option value="" disabled hidden>Choose a Semester</option>
-                    <option value="All">All</option>
-                    <option value="1st Semester">1st Semester</option>
-                    <option value="2nd Semester">2nd Semester</option>
-                    <option value="Summer">Summer</option>
-                    {/* Add more options for other semesters */}
-                  </select>
-                </div>
-              </div>
-              <div className="content-wrapper">
-                <div className="form-group">
-                  <select
-                    className="form-control"
-                    onChange={(e) => {
-                      setSelectedDay(e.target.value);
-                      filterSchedulesByDay(e.target.value);
-                    }}
-                    value={selectedDay}
-                  >
-                    <option value="" disabled hidden>Choose a Day</option>
-                    <option value="All">All</option>
-                    <option value="Mon/Wed">Mon/Wed</option>
-                    <option value="Tue/Thurs">Tue/Thurs</option>
-                    <option value="Fri/Sat">Fri/Sat</option>
-                    <option value="Monday">Monday</option>
-                    <option value="Tuesday">Tuesday</option>
-                    <option value="Wednesday">Wednesday</option>
-                    <option value="Thursday">Thursday</option>
-                    <option value="Friday">Friday</option>
-                    <option value="Saturday">Saturday</option>
-                    {/* Add more options for other days */}
-                  </select>
-                </div>
-              </div>
-            </div>
 
-            {loading ? (
-              <p>Loading...</p>
-            ) : facultySchedules.length === 0 ? (
-              <p>No schedules available.</p>
-            ) : (
-              <div className="table-responsive">
-                <table className="table">
-                  <thead>
-                    <tr>
-                      <th>School Year</th>
-                      <th>Semester</th>
-                      <th>Subject Code</th>
-                      <th>Subject Description</th>
-                      <th>Course</th>
-                      <th>Day</th>
-                      <th>Time</th>
-                      <th>Building</th>
-                      <th>Room</th>
-                      <th>Actions</th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    {facultySchedules.map((subject, index) => (
-                      <tr key={index}>
-                        <td>{subject.schoolYear}</td>
-                        <td>{subject.semester}</td>
-                        <td>{subject.subjectCode}</td>
-                        <td>{subject.subjectDescription}</td>
-                        <td>{subject.course}</td>
-                        <td>{subject.day}</td>
-                        <td>{subject.time}</td>
-                        <td>{subject.building}</td>
-                        <td>{subject.room}</td>
-                        <td>
-                          {roomOccupied ? (
-                            <button className="btn btn-danger" onClick={handleEndClass}>End Class</button>
-                          ) : attendingClass ? (
-                            <button className="btn btn-primary" onClick={handleAttendClass}>Attend Class</button>
-                          ) : (
-                            <button className="btn btn-success" onClick={() => handleOpenScanner(subject)}>Open Scanner</button>
-                          )}
-                        </td>
+              {loading ? (
+                <p>Loading...</p>
+              ) : facultySchedules.length === 0 ? (
+                <p>No schedules available.</p>
+              ) : (
+                <div className="table-responsive">
+                  <table className="table">
+                    <thead>
+                      <tr>
+                        <th>School Year</th>
+                        <th>Semester</th>
+                        <th>Subject Code</th>
+                        <th>Subject Description</th>
+                        <th>Course</th>
+                        <th>Day</th>
+                        <th>Time</th>
+                        <th>Building</th>
+                        <th>Room</th>
+                        <th>Actions</th>
                       </tr>
-                    ))}
-                  </tbody>
-                </table>
-              </div>
-            )}
-         
-      
+                    </thead>
+                    <tbody>
+                      {facultySchedules.map((subject, index) => (
+                        <tr key={index}>
+                          <td>{subject.schoolYear}</td>
+                          <td>{subject.semester}</td>
+                          <td>{subject.subjectCode}</td>
+                          <td>{subject.subjectDescription}</td>
+                          <td>{subject.course}</td>
+                          <td>{subject.day}</td>
+                          <td>{subject.time}</td>
+                          <td>{subject.building}</td>
+                          <td>{subject.room}</td>
+                          <td>
+                            {roomOccupied ? (
+                              <button className="btn btn-danger" onClick={handleEndClass}>End Class</button>
+                            ) : attendingClass ? (
+                              <button className="btn btn-primary" onClick={handleAttendClass}>Attend Class</button>
+                            ) : (
+                              <button className="btn btn-success" onClick={() => handleOpenScanner(subject)}>Open Scanner</button>
+                            )}
+                          </td>
+                        </tr>
+                      ))}
+                    </tbody>
+                  </table>
+                </div>
+              )}
+
+            </div>
           </div>
-         
+        </div>
 
-              </div>
-          </div>
+      </div>
 
-
-
-          </div>
-          
-          {showScanner && (
+      {showScanner && (
         <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center backdrop-blur-sm bg-gray-200 ">
           <div className="absolute bg-white p-6 rounded-md shadow-md w-3/4 sm:w-3/4 md:w-1/2 lg:w-1/4 border border-5 border-red-800 ">
             <h2 className="text-lg font-semibold mb-4">Open Scanner for:</h2>
@@ -417,7 +419,7 @@ function FacultySchedule() {
                 <p className="text-lg font-bold">{scanResult}</p>
               </div>
             )}
-            
+
             {/* Buttons and messages */}
             <div className="mt-4">
               {isScannerOpen && (
@@ -437,7 +439,7 @@ function FacultySchedule() {
         </div>
       )}
 
-  </div>
+    </div>
   );
 }
 
