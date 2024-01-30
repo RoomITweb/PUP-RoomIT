3dcc50df2a12999a22fd6192216bdff50cf68f44
RoomITweb <151912982+RoomITweb@users.noreply.github.com>
2024-01-30 10:30:40 +0800

    hev abi gu

--- roomit/src/Components/F_classSched.js
+++ roomit/src/Components/F_classSched.js
@@ -80,6 +80,7 @@ function FacultySchedule() {
           setRoomOccupied(true);
         } else {
           setRoomOccupied(false);
+          setAttendingClass(false);
         }
 
       } catch (error) {
